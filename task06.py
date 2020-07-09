4bcb0f49508b8cad106178c8c09333935485f2a5
alexnayg <67977611+alexnayg@users.noreply.github.com>
2020-07-09 22:12:02 +0300

    Delete task06.py

--- task06.py
+++
@@ -1,7 +0,0 @@
-current_value = int(input("Сколько метров вы можете пробежать сейчас?: "))
-future_value = int(input("К какому результату стремитесь?: "))
-days = 0
-while current_value <= future_value:
-    days += 1
-    current_value = current_value * 1.1
-print(f"Этот результат сможете достичь на {days} день")
